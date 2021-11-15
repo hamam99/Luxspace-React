3b2c18328d21f568a71f4d3a46b0e97fcfb4a753
Hamam <zero.saturnus@gmail.com>
2021-11-16 05:12:21 +0700

    create component homepage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import Header from '../parts/Header'
+
+export default function HomePage(props) {
+    return (
+        <Fragment>
+            <Header/>
+            <div class="container mx-auto py-5">
+                TESTOMG
+            </div>
+        </Fragment>
+    )
+}
