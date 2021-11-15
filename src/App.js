b29e5edf584fb523069a01f48a04f3ffedc15786
Hamam <zero.saturnus@gmail.com>
2021-11-16 05:12:56 +0700

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './assets/css/app.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-    <h1 className="text-xl ">Testing</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>,
     </div>
   );
 }
