1c865c3e2ca3246abf686c4f2ed05f3157ca05a0
Hamam <zero.saturnus@gmail.com>
2022-03-09 16:08:18 +0700

    integrate component with home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,20 @@
 import React, { Fragment } from 'react'
+import Clients from '../parts/Clients'
 import Header from '../parts/Header'
 import Hero from '../parts/Hero'
+import BrowseRoom from '../parts/HomePage/BrowseRoom'
+import JustArrived from '../parts/HomePage/JustArrived'
+import Sitemap from '../parts/Sitemap'
 
 export default function HomePage(props) {
     return (
         <Fragment>
             <Header/>
             <Hero/>
+            <BrowseRoom/>
+            <JustArrived/>
+            <Clients/>
+            <Sitemap/>  
         </Fragment>
     )
 }
